5ade96a048c25bb9cd011fca27944c9c06dbc3b1
Satish Prasad <spd61185@gmail.com>
2021-02-16 18:39:54 +0530

    Updated files

--- authorization-code-type-server/authorization-code-type-server/src/main/java/com/oauth/controller/BookPriceController.java
+++ authorization-code-type-server/authorization-code-type-server/src/main/java/com/oauth/controller/BookPriceController.java
@@ -1,8 +1,10 @@
-package com.oauth.entity;
+package com.oauth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.oauth.entity.BookPriceInfo;
 
 @RestController
 public class BookPriceController {
